e908afb075dc312f1cc39ff842a9f70fa382b30d
Robin <robinkorkmaz@hotmail.com>
2022-02-22 21:15:10 +0100

    add string calculator step 5

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,21 +1,28 @@
 package com.example;
 
 public class StringCalculator {
-    public int Add(String numbers)  {
+    public int Add(String numbers) throws Exception {
         int rst = 0;
 
         if (numbers == "")
             return rst;
 
-        if (numbers.startsWith("//")) 
-            numbers = numbers.substring(3).replaceAll(numbers.substring(2,3), ",");
+        if (numbers.startsWith("//"))
+            numbers = numbers.substring(3).replaceAll(numbers.substring(2, 3), ",");
 
         String[] numStrings = numbers.split(",|\n");
 
+        String errorMsg = "negatives not allowed: ";
+
         for (String num : numStrings) {
-            rst += Integer.parseInt(num);
+            int numVal = Integer.parseInt(num);
+            if (numVal < 0)
+                errorMsg += num + " ";
+            rst += numVal;
         }
 
+        if (errorMsg.length() > 23)
+            throw new Exception(errorMsg);
         return rst;
     }
 }
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -8,24 +8,30 @@ class StringCalculatorTest {
     StringCalculator sc = new StringCalculator();
 
     @Test
-    public void Step1Test() {
+    public void Step1Test() throws Exception {
         assertEquals(0, sc.Add(""));
         assertEquals(1, sc.Add("1"));
         assertEquals(5, sc.Add("2,3"));
     }
 
     @Test
-    public void Step2Test() {
+    public void Step2Test() throws Exception {
         assertEquals(15, sc.Add("4,5,6"));
     }
 
     @Test
-    public void Step3Test() {
+    public void Step3Test() throws Exception {
         assertEquals(6, sc.Add("1\n2,3"));
     }
 
     @Test
-    public void Step4Test() {
+    public void Step4Test() throws Exception {
         assertEquals(6, sc.Add("//;1\n2;3"));
+    }
+
+    @Test
+    public void Step5Test() {
+        Exception ex = assertThrows(Exception.class, ()-> sc.Add("-1,-2,3"));
+        assertTrue(ex.getMessage().contains("-1") && ex.getMessage().contains("-2"));
     }
 }
