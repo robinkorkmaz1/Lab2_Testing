b71f825fd0a0876338dbf5e1cd4599fc923f42dc
Robin <robinkorkmaz@hotmail.com>
2022-02-22 20:37:16 +0100

    add string calculator step 2

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -2,14 +2,17 @@ package com.example;
 
 public class StringCalculator {
     public int Add(String numbers)  {
+        int rst = 0;
+
         if (numbers == "")
-            return 0;
+            return rst;
 
         String[] numStrings = numbers.split(",");
 
-        if (numStrings.length == 1)
-            return Integer.parseInt(numStrings[0]);
-        else
-            return Integer.parseInt(numStrings[0]) + Integer.parseInt(numStrings[1]);
+        for (String num : numStrings) {
+            rst += Integer.parseInt(num);
+        }
+
+        return rst;
     }
 }
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator sc = new StringCalculator();
+
     @Test
     public void Step1Test() {
-        StringCalculator sc = new StringCalculator();
         assertEquals(0, sc.Add(""));
         assertEquals(1, sc.Add("1"));
         assertEquals(5, sc.Add("2,3"));
+    }
+
+    @Test
+    public void Step2Test() {
+        assertEquals(15, sc.Add("4,5,6"));
     }
 }
